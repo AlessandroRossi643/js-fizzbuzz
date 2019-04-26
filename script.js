1476e9ffd1e96d3f93833afd4425cf557b819390
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-04-26 17:01:20 +0200

    Cambiata struttura e migliorato aspetto visivo

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-// stampare tutti i numeri da 1 a 100
-var numeri=0;
-for (var i = 0; i < 100; i++) {
-  numeri++;
+// Creazione array vuoto
+var lista_numeri=[];
 
-// condizione per i multipli di 5
-  if (numeri%5==0) {
-    document.writeln("Buzz");
+//inserimento numeri nell'array
+for (var i = 1; i <= 100; i++) {
+
+  var li=document.createElement("LI");
+
+//condizione per i multipli di 3 e 5
+  if (i % 3 == 0 && i % 5 == 0) {
+    lista_numeri[i] = "FizzBuzz";
+
+//condizione per i multipli di 3
+  }
+  else if (i % 3 == 0) {
+    lista_numeri[i] = "Fizz";
+
   }
 
-// condizione per i multipli di 3
-  if (numeri%3==0) {
-    document.writeln("Fizz");
+//condizione per i multipli di 5
+  else if (i % 5 == 0) {
+    lista_numeri[i] = "Buzz";
   }
 
-// condizione per i multipli di 3 e 5
-  if (numeri%3==0 && numeri%5==0) {
-    document.writeln("FizzBuzz");
+  else {
+    lista_numeri[i] = i;
   }
 
-//Stampa finale
-  document.writeln(numeri + "<br>");
+  var numeri = document.createTextNode(lista_numeri[i]);
+   li.appendChild(numeri);
+
+
+//stampa finale
+  document.getElementById("listanumerihtml").appendChild(li);
 }
